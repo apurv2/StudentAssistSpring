c36216d0c817ff9776883a0dddb976817f858094
apurv <apurv2@gmail.com>
2017-12-28 02:47:32 -0500

    header Image and name

--- frontend/src/main/frontend/src/app/dashboard/flashCards/landing.flash.cards.ts
+++ frontend/src/main/frontend/src/app/dashboard/flashCards/landing.flash.cards.ts
@@ -3,14 +3,15 @@ import { Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 import { FlashCardsModel } from '../models/flash.cards.model';
 import { LandingFlashCardsService } from './landing.flash.cards.service';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'landing-flash-cards',
     templateUrl: 'landing.flash.cards.html'
 })
 export class LandingFlashCards {
-
+    flashCardIntervalObservable: Subscription;
     flashCardsData: FlashCardsModel;
 
     constructor(private router: Router,
@@ -19,9 +20,9 @@ export class LandingFlashCards {
 
     ngOnInit() {
         this.getFlashCards();
-        Observable.interval(10000).subscribe(x => {
-           this.getFlashCards();
-          });
+        this.flashCardIntervalObservable = Observable.interval(10000).subscribe(x => {
+            this.getFlashCards();
+        });
     }
 
     searchResultCardClick() {
@@ -31,7 +32,11 @@ export class LandingFlashCards {
     getFlashCards() {
         this.flashCardsService.getFlashCards()
             .subscribe(flashCards => {
-                this.flashCardsData = flashCards
+                this.flashCardsData = flashCards;
             });
+    }
+
+    ngOnDestroy() {
+        this.flashCardIntervalObservable.unsubscribe();
     }
 }
