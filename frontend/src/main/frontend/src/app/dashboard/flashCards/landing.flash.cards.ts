0ca207ef671405c2b7c8eb05ab8c818cad2bd9f4
Sai Kiran <Saikiran.pentukar@gmail.com>
2018-02-04 10:52:33 -0500

    flash cards airport functionality

--- frontend/src/main/frontend/src/app/dashboard/flashCards/landing.flash.cards.ts
+++ frontend/src/main/frontend/src/app/dashboard/flashCards/landing.flash.cards.ts
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subject } from 'rxjs/Subject';
-import { FlashCardsModel } from '../models/flash.cards.model';
-import { LandingFlashCardsService } from './landing.flash.cards.service';
-import {Observable} from 'rxjs/Rx';
-import { FlashCardsRequestModel } from '../models/flash.cards.request.model';
-import { SharedDataService } from '../../shared/data/shared.data.service';
-import { University } from '../../accommodation/shared/models/universities.list.model';
-
-@Component({
-    selector: 'landing-flash-cards',
-    templateUrl: 'landing.flash.cards.html'
-})
-export class LandingFlashCards {
-
-    flashCardsData: FlashCardsModel;
-
-    constructor(private router: Router,
-        private flashCardsService: LandingFlashCardsService,
-        private sharedDataService: SharedDataService) {
-            
-    }
-
-    ngOnInit() {
-        this.getFlashCards();
-        Observable.interval(10000).subscribe(x => {
-           this.getFlashCards();
-          });
-    }
-
-    searchResultCardClick() {
-        this.router.navigate(['/simple-search/']);
-    }
-
-    getFlashCards() {
-        let flashCardsRequestModel = new FlashCardsRequestModel();
-        let selectedUniversities = this.sharedDataService.getUserSelectedUniversitiesList();
-        flashCardsRequestModel.universityIDs = new Array(selectedUniversities.length);
-        for(let i=0;i<selectedUniversities.length;i++){
-            flashCardsRequestModel.universityIDs[i] = selectedUniversities[i].universityId;
-        }
-        flashCardsRequestModel.currentUniversityID = this.flashCardsData == null ? 0 : this.flashCardsData.currentUniversityID;
-        this.flashCardsService.getFlashCards(flashCardsRequestModel)
-            .subscribe(flashCards => { 
-                this.flashCardsData = flashCards
-            });
-    }
-}
-=======
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
@@ -91,4 +40,3 @@ export class LandingFlashCards {
         this.flashCardIntervalObservable.unsubscribe();
     }
 }
->>>>>>> branch 'master' of https://github.com/apurv2/StudentAssistSpring
